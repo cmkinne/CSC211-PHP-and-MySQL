5895cd86d1e3096ac02bbd5c437bcf64405e9739
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-01-24 17:10:58 -0700

    uploaded hello world with comment code

---
+++ helloworld with comments.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Hello, World!</title>
+</head>
+<body>
+
+<h1>My first PHP page</h1>
+
+<?php
+echo "Hello World!";
+
+// This is a single line comment
+
+/* This is what you use
+for a multi-line comment */
+?>
+
+
+
+</body>
+</html>
