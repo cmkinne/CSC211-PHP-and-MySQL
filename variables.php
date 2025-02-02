097ddcd4259aeac3299fcea78bd89dd6ab13f9b3
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-02-02 12:46:42 -0700

    added variables.php via upload

---
+++ variables.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Name, address and phone number:
+$f_name = "Chris";
+$l_name = "Kinne";
+$address_1 = "123 E Main St";
+$address_2 = "Tempe, AZ 85283";
+$phone = "(480)123-4567";
+
+// Print details
+print "<p>My name is:<br>$f_name $l_name</p>";
+print "<p>My address is:<br>$address_1<br>$address_2</p>";
+print "<p>My phone is:<br>$phone</p>";
+
+?>
+</body>
+</html>
