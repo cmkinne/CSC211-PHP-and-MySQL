511a39a3be3f2bc3dc6059db39cf8bcde9224a04
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-02-02 12:24:53 -0700

    Add predefined.php file via upload

---
+++ predefined.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Predfined Variables</title>
+</head>
+<body>
+<pre>
+<?php // Script 2.1 - predefined.php
+
+// Show the value of the $_SERVER variable:
+print_r($_SERVER);
+
+?>
+</pre> 
+</body>
+</html>
