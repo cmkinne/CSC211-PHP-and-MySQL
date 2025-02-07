0068a547b60e32317469a7deec7f03ee38b99580
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-02-07 11:46:38 -0700

    Update handle_calc2.php

--- handle_calc2.php
+++ handle_calc2.php
@@ -36,7 +36,7 @@
     $monthly = $total / $payments;
 	
 	// Apply the proper formatting:
-	$total = number_format($roral, 2);
+	$total = number_format($total, 2);
 	$monthly = number_format($monthly, 2);
 
     // Print out the results:
