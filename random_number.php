dac58ae4d433ea626c014f7caace113e8cdeaf81
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-02-07 12:32:50 -0700

    Add files via upload
    added random_number.php

---
+++ random_number.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Random Numbers</title>
+</head>
+<body>
+    <?php
+	// Generate random number between 10 and 100, assign to variable
+	$rand_number = rand(10, 100);
+	
+	// Echo the random number
+	echo $rand_number;
+	
+	?>
+</body>
+</html>
