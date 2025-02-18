e87eb4841efde6e7cc69abd30fa6333c469010d7
cmkinne <104061909+cmkinne@users.noreply.github.com>
2025-02-18 16:17:56 -0700

    add file via upload
    Added books.php
    **NOTE: this overrode the books.php from earlier in the semester

--- books.php
+++ books.php
@@ -1,44 +1,21 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Larry Ullman's Books and Chapters</title>
-</head>
-<body>
-    <h1>Some of Larry Ullman's Books</h1>
-    <?php // Script 7.4 - books.php
-    /* This script creates and prints out a multidimensional array. */
+<?php // Script 8.5 - books.php
+/* This page lists J.D. Salinger's bibliography. */
 
-    // Address error management
-    ini_set('display_errors', 1);
+// Set the page title and include the header file:
+define('TITLE', 'Books by J.D. Salinger');
+include('templates/header.html');
 
-    // Create the first array
-    $phpvqs = [1 => 'Getting Started with PHP', 'Variables', 'HTML Forms and PHP', 'Using Numbers'];
+// Leave the PHP section to display lots of HTML:
+?>
 
-    // Create the second array
-    $phpadv = [1 => 'Advanced PHP Techniques', 
-    'Developing Web Applications', 'Advanced Database Concepts', 'Basic Object-Oriented Programming'];
+<h2>J.D. Salinger's Books</h2>
+<ul>
+    <li>The Catcher in the Rye</li>
+    <li>Nine Stories</li>
+    <li>Franny and Zooey</li>
+    <li>Raise High the Roof Beam, 
+        Carpenters and Seymour: An Introduction</li>
+</ul>
 
-    // Create the third array
-    $phpmysql = [1=> 'Introduction to PHP', 'Programming with PHP', 'Creating Dynamic Web Sites', 'Introduction to MySQL'];
-
-    // Create the multidimensional array
-    $books = [
-        'PHP VQS' => $phpvqs,
-        'PHP Advanced VQP' => $phpadv,
-        'PHP and MySQL VQP' => $phpmysql
-    ];
-
-    // Print out some values
-    print "<p>The third chapter of my first book is <i>{$books['PHP VQS'][3]}</i>.</p>";
-    print "<p>The first chapter of my second book is <i>{$books['PHP Advanced VQP'][1]}</i>.</p>";
-    print "<p>The fourth chapter of my fourth book is <i>{$books['PHP and MySQL VQP'][4]}</i>.</p>";
-
-    // See what happens with foreach:
-    foreach ($books as $key => $value) {
-        print "<p>$key: $value</p>\n";
-    }
-
-    ?>
-</body>
-</html>
+<?php include('templates/footer.html')
+?>
